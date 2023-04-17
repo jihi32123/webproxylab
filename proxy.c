231743cfc37309bddb6f6d338293f3094b4b4e50
jihi32123 <jihi32123@gmail.com>
2023-04-17 10:33:21 +0000

    first commit

--- proxy.c
+++ proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -9,7 +10,165 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main() {
-  printf("%s", user_agent_hdr);
-  return 0;
+
+void read_header(int fd, char*host, char* port, char* url);
+void read_requesthdrs(rio_t *rp);
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg);
+void doit(int fd);
+void send_request(char* host, char* port, char* uri, int connfd);
+
+int main(int argc, char **argv) 
+{
+  int listenfd, connfd;
+  char hostname[MAXLINE], port[MAXLINE];
+  char buf[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
+
+  /* Check command line args */
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  listenfd = Open_listenfd(argv[1]);
+
+  while (1) {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+    printf("connect\n");
+    int optval = 1;
+    Setsockopt(connfd, SOL_SOCKET, SO_REUSEADDR,(const void *)&optval, sizeof(int));
+    Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
+                port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s)\n", hostname, port);
+    doit(connfd); 
+    Close(connfd);                                         //line:netp:tiny:close
+  }
 }
+
+/* 주요 로직 */
+void doit(int connfd){
+  /* 1. 클라이언트한테 받은 요청의 헤더를 읽어서 저장하자  */   
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE]; // 헤더 정보 저장할 변수들
+  rio_t rio;
+
+  Rio_readinitb(&rio, connfd);                                    // Rio I/O 초기화
+  if (!Rio_readlineb(&rio, buf, MAXLINE))                         // Rio I/O 못 읽으면 반환 
+      return;
+  printf("%s", buf);                                              // 헤더 전부 출력
+  sscanf(buf, "%s %s %s", method, uri, version);                  // 헤더 읽기                                       
+  read_requesthdrs(&rio);                                         // 남은 헤더 처리하기 
+
+  /* 2. 받은 헤더 URI를 파싱하자 */
+  char *end_host[MAXLINE], *end_port[MAXLINE], *end_uri[MAXLINE];   // 파싱해서 저장할 정보들
+  parse_uri(uri, end_host, end_port);                        // 파싱하기
+
+  /* 3. 파싱한 URI를 재구성해서 서버에 전송하자 */
+  send_request(end_host, end_port, uri, connfd);
+
+  /* 4. 서버에서 받은 응답을 클라이언트에게 보내주자 */
+
+}
+
+/* 헤더 처리하는 함수 */
+void read_requesthdrs(rio_t *rp) 
+{
+  char buf[MAXLINE];
+
+  Rio_readlineb(rp, buf, MAXLINE);
+  printf("%s", buf);
+  while(strcmp(buf, "\r\n")) {          //line:netp:readhdrs:checkterm
+    Rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+  }
+  return;
+}
+
+/* $begin parse_uri */
+int parse_uri(char *uri, char *host, char *port) 
+{
+  // char *ptr;
+  char *token;
+  
+  // host 찾기
+  token = strtok(uri, "/"); // "//" 뒤 문자열은 호스트 명
+  token = strtok(NULL, ":");
+  strcpy(host,++token);
+  
+  // port 찾기
+  token = strtok(NULL, "/"); // ":"로 구분된 포트 찾기
+  if(token != NULL){  
+    strcpy(port,token);
+  }
+  else{
+    strcpy(port, "80");  // 포트가 없는 경우 기본값 80으로 설정
+    printf("token = %s\n", token);
+  }
+
+  // uri 경로 찾기
+  token = strtok(NULL, "");
+  if (token != NULL) {
+    strcpy(uri, token);
+  } else {
+    strcpy(uri, "/");  // uri 경로가 없는 경우 "/"로 설정
+  }
+}
+
+void send_header(int fd, char *uri, int connfd) 
+{
+  char buf[MAXLINE];
+
+  /* Print the HTTP response headers */
+  sprintf(buf, "GET /%s HTTP/1.1\r\n\r\n", uri);
+  Rio_writen(fd, buf, strlen(buf));
+  
+  rio_t rio;
+
+  Rio_readinitb(&rio, fd);                            
+  while (Rio_readlineb(&rio, buf, MAXLINE)!= 0) {
+    printf("%s \n", buf);
+    Rio_writen(connfd, buf, strlen(buf));
+  }
+}
+
+
+/* tiny 서버에 요청하는 함수*/
+void send_request(char *host, char *port, char *uri, int fd) 
+{
+    int clientfd; // 소켓 식별자
+    char buf[MAXLINE]; // 값 저장할 애들
+    rio_t rio;
+    // port = strtok(end_host , "/");
+    printf("host = <%s>, port =<%s>, url =<%s>\n", host, port, uri);
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+    send_header(clientfd,uri, fd);
+    Close(clientfd); //line:netp:echoclient:close
+}
+/* $end proxy */
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg) 
+{
+    char buf[MAXLINE];
+
+    /* Print the HTTP response headers */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+
+    /* Print the HTTP response body */
+    sprintf(buf, "<html><title>Tiny Error</title>");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<body bgcolor=""ffffff"">\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "%s: %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<p>%s: %s\r\n", longmsg, cause);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<hr><em>The Tiny Web server</em>\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+}
+/* $end clienterror */
